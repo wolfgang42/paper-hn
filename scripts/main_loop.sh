847a277d43e11eb85a0a7593f81784ed987c0c20
Mihai Galos <mihaigalos@gmail.com>
2020-10-19 22:00:51 +0200

    Use refresh interval variable defaulting to 300 seconds

--- scripts/main_loop.sh
+++ scripts/main_loop.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+
+REFRESH_INTERVAL=${REFRESH_INTERVAL:-300}
 cd /paper-hn
 
 http-server . >http.log 2>&1 &
@@ -8,7 +10,7 @@ echo Starting up... > index.html
 while true
 do
     node --experimental-modules ./bin/generate-html.mjs    
-    sleep 300
+    sleep $REFRESH_INTERVAL
     rm cache/hn/*.json
 done
 
