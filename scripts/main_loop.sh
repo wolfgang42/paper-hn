552ae6d2a66f438fea97541785fd1071514c38d3
Mihai Galos <mihaigalos@gmail.com>
2020-10-19 21:51:38 +0200

    Dockerize Paper Hacker News

---
+++ scripts/main_loop.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cd /paper-hn
+
+http-server . >http.log 2>&1 &
+echo Starting up... > index.html
+
+while true
+do
+    node --experimental-modules ./bin/generate-html.mjs    
+    sleep 60
+    rm cache/hn/*.json
+done
+
