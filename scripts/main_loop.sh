61e1f3f40faa98573112e724f5598fd24fade0d7
Mihai Galos <mihai@galos.one>
2024-02-24 19:27:30 +0100

    chore: Docker - Improve verbosity

--- scripts/main_loop.sh
+++ scripts/main_loop.sh
@@ -9,6 +9,7 @@ echo Generating index.html .. > index.html
 while true
 do
     echo "-------------------------------------------"
+    echo "Start: $(date)"
     node --trace-warnings --experimental-modules ./bin/generate-html.mjs --hacker-news=hn 2>&1
     echo "Done: $(date)"
     sleep $REFRESH_INTERVAL
@@ -16,6 +17,7 @@ do
     mkdir -p /home/user/cache/hn2
     rm -rf /home/user/cache/hn2
     echo "-------------------------------------------"
+    echo "Start: $(date)"
 
     node --trace-warnings --experimental-modules ./bin/generate-html.mjs --hacker-news=hn2 2>&1
     rm -rf /home/user/cache/hn
